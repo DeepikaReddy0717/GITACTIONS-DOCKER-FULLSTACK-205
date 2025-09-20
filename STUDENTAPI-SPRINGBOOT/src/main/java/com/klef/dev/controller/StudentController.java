6fb33d126ea121d320969da2fc3244246f9f6144
DeepikaReddy <deepikareddymandapati@gmail.com>
2025-09-20 07:37:14 +0530

    Projectv2.0

--- STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
+++ STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
@@ -3,13 +3,20 @@ package com.klef.dev.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.klef.dev.entity.Student;
 import com.klef.dev.service.StudentService;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/studentapi/")
@@ -22,8 +29,14 @@ public class StudentController {
     @GetMapping("/")
     public String home() 
     {
-        return "Docker Full Stack Deployment Demo";
+        return "Docker Backend is running";
     }
+    @GetMapping("/docker")
+    public String docker() 
+    {
+        return "Docker Full Stack Deployment Demo with git Actions";
+    }
+
     
 
     @PostMapping("/add")
